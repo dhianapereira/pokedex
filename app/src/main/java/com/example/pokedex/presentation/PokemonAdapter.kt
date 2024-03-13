a91247ff99fd8f746f165845acbc0e5c5b5fab22
dhianapereira <dhianaslv@outlook.com>
2024-03-13 14:46:31 -0300

    feat: Load pokemon image

--- app/src/main/java/com/example/pokedex/presentation/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/presentation/PokemonAdapter.kt
@@ -3,6 +3,9 @@ package com.example.pokedex.presentation
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.pokedex.R
 import com.example.pokedex.databinding.PokemonItemBinding
 import com.example.pokedex.domain.Pokemon
 
@@ -13,6 +16,11 @@ class PokemonAdapter(private val items: List<Pokemon>) :
         fun bind(pokemon: Pokemon) {
             binding.name.text = pokemon.name
             binding.number.text = pokemon.formattedNumber()
+            Glide.with(binding.root.context)
+                .load(pokemon.getImage())
+                .error(R.drawable.image_not_supported)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(binding.image)
         }
     }
 
